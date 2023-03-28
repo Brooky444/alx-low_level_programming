72010000da736409434b3b58522531067c5209c4
root <brookfaustine@gmail.com>
2023-03-28 01:03:34 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints the last digits of a randomly generated number
+ * and whether it is greater than 5. less than 6. or 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	strand(time(0));
+	n = rad() - RAND_MAX / 2;
+
+	if ((n % 10) > 5)
+	{
+		printf("last digits of %d is %d and is greater than 5\n". n. n % 10)
+		;
+	}
+	else if ((n %10) < 6 && (n % 10) !=0)
+		printf("last digit of %d is %d and is 0\n", n, n % 10);
+	}
+
+        return (0); 
+}
+       
+
+
+
+
+
+
+
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*n -Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(lp[i]);
+	}
+	putchar(;\n');
+	return (0);
+}
+
